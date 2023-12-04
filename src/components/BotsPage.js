d7f841e2ccc3eed9dbd25583476a95180de71ff3
Mohammed <khamismitch@gmail.com>
2023-12-04 07:23:16 +0300

    additional progress

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -5,6 +5,37 @@ import BotCollection from "./BotCollection";
 function BotsPage() {
   //start here with your code for step one
   const [selectedBots, setSelectedBots] = useState([]);
+  function handleAddToArmy(bot) {
+    setSelectedBots([...selectedBots, bot]);
+  }
+  function handleDischarge(bot) {
+    setSelectedBots(
+      selectedBots.filter((selectedBot) => selectedBot.id !== bot.id)
+    );
+
+    fetch(` http://localhost:8002/${bot.id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then(() => selectedBots(bot))
+      .catch((err) => {
+        console.log("Error", err);
+      });
+  }
+
+  function handleReleaseFromArmy(bot) {
+    setSelectedBots(
+      selectedBots.filter((selectedBot) => selectedBot.id !== bot.id)
+    );
+    fetch(` http://localhost:8002/${bot.id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then(() => selectedBots(bot))
+      .catch((err) => {
+        console.log("Error", err);
+      });
+  }
   return (
     <div>
       <YourBotArmy
@@ -18,38 +49,6 @@ function BotsPage() {
       />
     </div>
   );
-  function handleAddToArmy(bot) {
-    setSelectedBots([...selectedBots, bot]);
-  }
-  function handleDischarge(bot) {
-    setSelectedBots(
-      selectedBots.filter((selectedBot) => selectedBot.id !== bot.id)
-    );
-
-    fetch(` http://localhost:8002/${bot.id}`, {
-      method: "DELETE",
-    })
-      .then((res) => res.json())
-      .catch((err) => {
-        console.log("Error", err);
-      });
-  }
-
-  function handleReleaseFromArmy(bot) {
-    // Remove the bot from the selectedBots array
-    setSelectedBots(
-      selectedBots.filter((selectedBot) => selectedBot.id !== bot.id)
-    );
-    fetch(` http://localhost:8002/${bot.id}`, {
-      method: "DELETE",
-    })
-      .then((res) => res.json())
-      .then(()=>selectedBots(bot))
-      .catch((err) => {
-        console.log("Error", err);
-      });
-   
-  }
 }
 
 export default BotsPage;
